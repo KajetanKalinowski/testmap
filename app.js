ec9f5329f823d742e9487ef176cdb40f558edbb3
KajetanKalinowski <kajtul999@gmail.com>
2023-09-08 15:06:31 +0200

    nic nie wyszukuj

--- app.js
+++ app.js
@@ -1,73 +1,102 @@
-async function getData(){
-const data = await fetch("https://restcountries.com/v3.1/all")
-const json = await data.json()
-console.log(json)
-namesList(json)
+var json;
+async function getData()
+{
+    const data = await fetch("https://restcountries.com/v3.1/all")
+    json = await data.json()
+    console.log(json)
+    displayNames(json)
 }
 getData()
-function namesList(json){
+function displayNames(json){
     for(var i=0;i<=json.length-1;i++){
-        const addDiv = document.createElement('div')
-        addDiv.style.display = "grid"
-        addDiv.style.gridTemplateColumns = "33% 33% 34%"
-        addDiv.style.gridTemplateRows = "50px 50px"
-        addDiv.style.gridTemplateAreas = `"flaga nazwa nazwa" "flaga populacja stolica"`
-        document.getElementById('d2').appendChild(addDiv)
-        addDiv.id = "newDiv"
-
-        const flaga = document.createElement('img')
-        flaga.src = json[i].flags.png
-        flaga.style.height = "100px"
-        flaga.style.width = "100px"
-        flaga.style.gridArea = "flaga"
-        addDiv.appendChild(flaga)
-
-        
+        //div
+        const div = document.createElement('div')
+        div.classList.add('divs')
+        div.style.display = "grid"
+        div.style.gridTemplateColumns = "33% 33% 34%"
+        div.style.gridTemplateAreas = `"flaga nazwa nazwa" "flaga populacja stolica"`
+        div.style.width = "400px"
+        div.style.height = "100px"
+        document.getElementById("main").appendChild(div)
+        //flaga
+        const flag = document.createElement('img')
+        flag.style.width = "100px"
+        flag.style.height = "100px"
+        flag.src = json[i].flags.png
+        flag.style.gridArea = "flaga"
+        div.appendChild(flag)
+        //nazwa 
         
         const nazwa = document.createElement('h2')
-        nazwa.style.width = "200px"
-        nazwa.style.height = "50px"
-        nazwa.style.margin= "0px"
-        nazwa.style.color = "black"
+        nazwa.style.textAlign = "center"
         nazwa.innerHTML = json[i].name.common
         nazwa.style.gridArea = "nazwa"
-        addDiv.appendChild(nazwa)
-
-        const pop = document.createElement('h2')
-        pop.style.width = "100px"
-        pop.style.height = "50px"
-        pop.style.margin= "0px"
-        pop.innerHTML = json[i].population
-        pop.style.gridArea = "populacja"
-        addDiv.appendChild(pop)
-
-        const st = document.createElement('h2')
-        st.style.width = "100px"
-        st.style.height = "50px"
-        st.style.margin= "0px"
-        st.innerHTML = json[i].capital
-        st.style.gridArea = "stolica"
-        addDiv.appendChild(st)
+        div.appendChild(nazwa)
+        //populacja
+        const populacja = document.createElement('h2')
+        populacja.style.textAlign = "center"
+        populacja.innerHTML = json[i].population
+        populacja.style.gridArea = "populacja"
+        div.appendChild(populacja)
+        //stolica
+        const stolica = document.createElement('h2')
+        stolica.style.textAlign = "center"
+        stolica.innerHTML = json[i].capital
+        stolica.style.gridArea = "stolica"
+        div.appendChild(stolica)
         
-        
-        const ch1 = document.getElementById("ch1")
-        const ch2 = document.getElementById("ch2")
-        const ch3 = document.getElementById("ch3")
-        const ch4 = document.getElementById("ch4")
-        const ch5 = document.getElementById("ch5")
-        const ch6 = document.getElementById("ch6")
-        const region = json[i].region
-        ch1.addEventListener('change',(region)=>{
-            if(region == "Europ"){
-                addDiv == region
-                
-            }else{
-                hidden = addDiv
-            }
-
+    }
+}
+var licznik = 0
+var tab = []
+function check(id){
+    if(document.getElementById(id).checked == true){
+        licznik++
+        tab.push(id)
+        console.log(id)
+    }
+    else{
+        tab = tab.filter(function(el){
+            return el !=id
         })
-
-
-
+        licznik--
+    }
+    for(var i=0;i<=json.length-1;i++){
+        var isIt = false
+        for(var j=0;j<=licznik;j++){
+            
+            if(tab[j]==json[i].region){
+                isIt=true
+            }
+        }
+        console.log(isIt)
+        if(isIt!=true && licznik !=0){
+            document.getElementsByClassName('divs')[i].style.display = "none"
+        }
+        else{
+            document.getElementsByClassName('divs')[i].style.display = "grid"
+        }
+    }
+    console.log(licznik)
+}
+var inputText
+function search(){
+    inputText = document.getElementById('search').value
+    console.log(inputText)
+    for(var i=0;i<=json.length-1;i++){
+        var isthat = false
+        for(var j=0;j<=tab.length;j++){
+            
+            if(json[i].name.common.toLowerCase().includes(inputText.toLowerCase())&& json[i].region==tab[j]){
+                isthat = true
+            }
+            
+        }
+        if(isthat==false){
+            document.getElementsByClassName('divs')[i].style.display = "none"
+        }
+        else{
+            document.getElementsByClassName('divs')[i].style.display = "grid"
+        }
     }
 }
